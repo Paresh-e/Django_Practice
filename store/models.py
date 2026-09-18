5d502fb02a6794bdf0b1c52527def39729718734
Paresh <eshraghyparsa@gmail.com>
2026-09-18 14:25:52 +0330

    maked Product & Customer models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    Title = models.CharField(max_length=255)
+    Description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    inventory = models.IntegerField()
+    last_Update = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    birth_date = models.DateField(null=True)
